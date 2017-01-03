3c271ed92a1788e1971482048c355cd3fa856641
sveltedonut <harryjiang0114@gmail.com>
2017-01-03 12:56:53 -0500

    added images

--- dropyourpants.js
+++ dropyourpants.js
@@ -1,12 +1,15 @@
 var systems;
-var img;
-var colors = ["#0000EF", "#00EF00", "#EF0000", "#00EFEF", "#EF00EF", "#EFEF00"];
+var img = [null, null, null];
+var colors = ["#0000DD", "#00DD00", "#DD0000", "#00DDDD", "#DD00DD", "#DDDD00"];
 var changeColor = 0;
 var currentColor = 0;
+var onOff = 0;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  img = loadImage("assets/particle1.png");  // Load the image
+  img[0] = loadImage("assets/particle1.png");  // Load the image
+  img[1] = loadImage("assets/particle2.png");  // Load the image
+  img[2] = loadImage("assets/particle3.png");  // Load the image
   systems = [];
   this.p = new ParticleSystem(createVector(0, height));
   systems.push(p);
@@ -15,24 +18,34 @@ function setup() {
 }
 
 function draw() {
-  background(colors[currentColor]);
-  currentColor = changeColor == 9 ? int(random(0, 6)) : currentColor;
-  changeColor = changeColor == 9 ? 0 : changeColor + 1;
+  if (onOff) {
+    background(colors[currentColor]);
+    if (changeColor == 9) {
+      var oldColor = currentColor;
+      currentColor = int(random(0, 5));
+      currentColor = currentColor >= oldColor ? currentColor + 1 : currentColor;
+      changeColor = 0;
+    } else {
+      changeColor++;
+    }
+  } else {
+    background(255);
+  }
   for (i = 0; i < systems.length; i++) {
     systems[i].run();
-    systems[i].addParticle();
-  }
-  if (systems.length==0) {
-    fill(255);
-    textAlign(CENTER);
-    textSize(32);
-    text("click mouse to add particle systems", width/2, height/2);
+    if (onOff) {systems[i].addParticle();}
   }
 }
 
 function mousePressed() {
-  this.p = new ParticleSystem(createVector(mouseX, mouseY));
-  systems.push(p);
+  if (onOff == 0){
+    onOff = 1;
+    setTimeout(turnOff, 5000);
+  }
+}
+
+function turnOff() {
+  onOff = 0;
 }
 
 // A simple Particle class
@@ -40,6 +53,7 @@ var Particle = function(position) {
   this.acceleration = createVector(0, 0.08);
   this.velocity = createVector(random(-5, 5), random(-15, -8));
   this.position = position.copy();
+  this.img = int(random(0, 3));
   this.size = random(50, 150);
   this.lifespan = 512.0;
   this.theta = 0.0;
@@ -63,7 +77,7 @@ Particle.prototype.display = function () {
   push();
   translate(this.position.x, this.position.y);
   rotate(this.theta);
-  image(img, -this.size/2, -this.size/2, this.size, this.size);
+  image(img[this.img], -this.size/2, -this.size/2, this.size, this.size);
   pop();
 };
 
